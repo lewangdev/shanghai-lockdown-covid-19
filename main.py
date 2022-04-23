e3236a6db4cf92fa5dbd3ca1111da663146e385a
Le Wang <lewang.dev@gmail.com>
2022-04-23 14:42:38 +0800

    rename data

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     if os.path.exists(url_filename):
         urls = json.loads(read_file(url_filename))
 
+    urls_crawled = []
     pages = ['']
     for p in pages:
         url = 'https://wsjkw.sh.gov.cn/yqtb/index%s.html' % p
@@ -53,11 +54,11 @@ if __name__ == '__main__':
             hyperlink_html_content = get_html_content(target_url)
             filename = "%s.html" % hashlib.md5(
                 hyperlink_html_content.encode('utf8')).hexdigest()
-            urls.insert(
-                0, {"url": target_url, "text": hyperlink_text, "filename": filename})
+            urls_crawled.append(
+                {"url": target_url, "text": hyperlink_text, "filename": filename})
 
             write_file(hyperlink_html_content, "archived_html/" +
                        filename)
-
-    write_file(json.dumps(urls, ensure_ascii=False, indent=4,
+    urls_crawled.extend(urls)
+    write_file(json.dumps(urls_crawled, ensure_ascii=False, indent=4,
                separators=(',', ':')), url_filename)
