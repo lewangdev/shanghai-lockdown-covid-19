cd47fe085b9d9b7292a092762867a033f6cece69
Le Wang <lewang.dev@gmail.com>
2022-05-21 22:36:10 +0800

    disable generate db files

--- main.py
+++ main.py
@@ -34,5 +34,5 @@ if __name__ == '__main__':
     generate_places(cases)
 
     # Create db files when new urls crawled
-    if len_urls > 0:
-        create_db_files()
+    # if len_urls > 0:
+    #    create_db_files()
